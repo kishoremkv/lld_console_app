3d12959c3f3cfdb53b82c5a0821eeae4faffda24
Kishore <krishnaveerakishoremuppana@gmail.com>
2025-04-23 23:12:56 +0530

    Added Java console app

---
+++ src/main/java/BookMyShowLld/BookMyShowLldMain.java
@@ -0,0 +1,16 @@
+package BookMyShowLld;
+
+// Entities
+
+class Movie
+{
+    int id;
+    String name;
+
+}
+
+public class BookMyShowLldMain {
+    public static void main(String[] args){
+
+    }
+}
